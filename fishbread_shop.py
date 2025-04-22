d7a1365b0aff16d95035827a734d40f6fa72223d
siminju <djsoifd19@naver.com>
2025-04-22 19:14:10 +0900

    ✨ 맛 오입력 검출, 주문받은 붕어빵 판매 기능, 판매된 붕어빵 개수 저장하는 기능
    화이팅

--- fishbread_shop.py
+++ fishbread_shop.py
@@ -1,7 +1,31 @@
+stock = {
+    "팥붕어빵" : 10, 
+    "슈크림붕어빵" : 8,
+    "초코붕어빵" : 5
+}
 
+sales = {
+    "팥붕어빵" : 0,
+    "슈크림붕어빵" : 0,
+    "초코붕어빵" : 0
+}
 
-
-while True:
+def order_bread ():
+    while True: 
+        bread_type = input("주문할 붕어빵을 선택하세요(팥붕어빵, 슈크림붕어빵, 초코붕어빵) 안에 뒤로가길 원하시면 뒤로가기")
+        if bread_type == "뒤로가기":
+            break
+        if bread_type in stock:
+            bread_count = int(input("주문할 개수를 입력하세요: "))
+            if stock[bread_type] >= bread_count: 
+                stock[bread_type] -= bread_count
+                stock[bread_type] += bread_count
+                print(f"{bread_type} {bread_count}개가 판매되었습니다")
+            else: 
+                print(f"재고가 부족합니다. 현재 {stock[bread_type]}개만 주문 가능합니다.")
+        else:
+            print("정신을 똑바로 차리시고 현재 {stock}[bread_type]")
+while True:    
     mode = input("원하는 모드를 선택하세요(주문, 관리자, 종료): ")
     #mode = "종료"
     if mode == "종료":
